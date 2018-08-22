a033c74e63e7a3d53b939b5e48964d9ff345a20f
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-08-22 15:55:31 +0300

    read the databse.ini file to return conn params

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+from configparser import ConfigParser
 
 
 class BaseConfig(object):
@@ -37,3 +38,19 @@ app_config = {
     'testing': TestingConfig,
     'production': ProductionConfig
 }
+
+def config(filename = 'database.ini', section = 'postgresql'):
+    #create parser
+    parser = ConfigParser()
+    #read the config file
+    parser.read(filename)
+
+    #get section default to postgresql
+    db ={}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in  params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('section {0} not found in the {1} file' .format(section, filename))
+    return db
