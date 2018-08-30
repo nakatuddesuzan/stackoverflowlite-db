1c296479e1f52ccbb1e4dda26c327b5511df7dc1
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-08-30 12:45:00 +0300

    Modify database connection context manager

--- app/api/db_manager/db_config.py
+++ app/api/db_manager/db_config.py
@@ -9,7 +9,7 @@ class DatabaseConnection():
 
     def __enter__(self):
         try:
-            if os.getenv('APP_SETTINGS') == "testing":
+            if app_config['testing']:
                 self.conn = psycopg2.connect("dbname = 'test_stack_db', user = 'postgres' host = 'localhost' password = 'graphics123456789' port = '5432'")
                 self.cursor = self.conn.cursor()
                 return self.cursor
