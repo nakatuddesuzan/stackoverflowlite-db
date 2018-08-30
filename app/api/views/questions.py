a96309b7f6efb93def982a500121a132320678fd
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-08-30 19:31:18 +0300

    Modify questions views

--- app/api/views/questions.py
+++ app/api/views/questions.py
@@ -10,28 +10,26 @@ questions = Blueprint('questions', __name__)
 
 @questions.route('/api/v1/questions', methods=['POST'])
 @login_required
-def post_question(user_id):
+def post_question(user):
     try:
         if not request.get_json():
             return make_response(jsonify({"message": "Request should be json"}), 400)
-        title = request.get_json()['title']
-        subject = request.get_json()['subject']
-        qtn_desc = request.get_json()['qtn_desc']
-        user_id = request.get_json()['user_id']
-        question = Question('qtn_id', user_id=user_id, title=title, subject=subject, qtn_desc=qtn_desc)
+        title = request.get_json()['title'].strip()
+        subject = request.get_json()['subject'].strip()
+        qtn_desc = request.get_json()['qtn_desc'].strip()
+        question = Question('qtn_id', user.user_id, title=title, subject=subject, qtn_desc=qtn_desc)
         
         Question.create_questions_table(questions)
         result = Question.create_question(question)
         return result
     except Exception as e:
-        logging.error(e)
-        return make_response(jsonify({'message': str(e)}), 500)
+        raise e
 
 @questions.route('/api/v1/questions', methods=['GET'])
 @login_required
-def get_all_questions(user_id):
+def get_all_questions(user):
     try:
-        output = Question.retrieve_all_questions(user_id)
+        output = Question.retrieve_all_questions(user.user_id)
         print(output)
         return  make_response(jsonify(output), 200)
     except Exception as e:
@@ -42,12 +40,10 @@ def get_all_questions(user_id):
 @login_required
 def edit_question(user_id, qtn_id):
     try:
-        if not request.get_json():
-            return make_response(jsonify({"message": "Request should be json"}), 400)
-        title = request.get_json()['title']
-        subject = request.get_json()['subject']
-        qtn_desc = request.get_json()['qtn_desc']
-        qtn_id = request.get_json()['qtn_id']
+        title = request.get_json()['title'].strip()
+        subject = request.get_json()['subject'].strip()
+        qtn_desc = request.get_json()['qtn_desc'].strip()
+        qtn_id = request.get_json()['qtn_id'].strip()
 
         Question.update_qtn(qtn_id, title, subject, qtn_desc)
         return make_response(jsonify({"Message":'Succesfully Updated'}), 201)
@@ -57,9 +53,9 @@ def edit_question(user_id, qtn_id):
 
 @questions.route('/api/v1/question/<int:qtn_id>', methods=['DELETE'])
 @login_required
-def del_qtn(user_id, qtn_id):
+def del_qtn(user, qtn_id):
     try:
-        output = Question.delete_question(qtn_id, user_id)
+        output = Question.delete_question(qtn_id, user.user_id)
         return make_response(jsonify(output), 200)
     except Exception as e:
         logging.error(e)
@@ -67,9 +63,9 @@ def del_qtn(user_id, qtn_id):
 
 @questions.route('/api/v1/question/<int:qtn_id>', methods=['GET'])
 @login_required
-def get_one_question(user_id, qtn_id):
+def get_one_question(user, qtn_id):
     try:
-        output = Question.fetch_by_id(user_id, qtn_id)
+        output = Question.fetch_by_id(user.user_id, qtn_id)
         return make_response(jsonify({"question": output}), 200)
     except Exception as e:
         logging.error(e)
